e3e3b295d167a50ee4eb6d1a233bb49408ea6129
Gabriel Bull <me@gabrielbull.com>
2014-10-14 19:59:36 -0400

    Added method to obtain the browser session controller from the trait's parent class

--- src/BrowserSession/BrowserSessionTrait.php
+++ src/BrowserSession/BrowserSessionTrait.php
@@ -3,24 +3,27 @@ namespace Sinergi\BrowserSession;
 
 trait BrowserSessionTrait
 {
-    private $browserSessionController;
-
     /**
      * @return BrowserSessionController
+     * @throws \Exception
      */
-    protected function getBrowserSessionController()
+    private function tryGetBrowserSessionController()
     {
-        return $this->browserSessionController;
-    }
+        $browserSessionController = null;
+        if (method_exists($this, 'getContainer')) {
+            $container = call_user_func([$this, 'getContainer']);
+            if (isset($container['browserSessionController'])) {
+                $browserSessionController = $container['browserSessionController'];
+            }
+        } elseif (method_exists($this, 'getBrowserSessionController')) {
+            $browserSessionController = call_user_func([$this, 'getBrowserSessionController']);
+        }
 
-    /**
-     * @param BrowserSessionController $browserSessionController
-     * @return $this
-     */
-    protected function setBrowserSessionController(BrowserSessionController $browserSessionController)
-    {
-        $this->browserSessionController = $browserSessionController;
-        return $this;
+        if ($browserSessionController instanceof BrowserSessionController) {
+            return $browserSessionController;
+        }
+
+        throw new \Exception('You need to provide the trait a method to get the BrowserSessionController');
     }
 
     /**
@@ -28,24 +31,27 @@ trait BrowserSessionTrait
      */
     protected function getBrowserSession()
     {
-        if (!$session = $this->getBrowserSessionController()->getSession()) {
-            $session = $this->getBrowserSessionController()->createSession();
+        $browserSessionController = $this->tryGetBrowserSessionController();
+        if (!$session = $browserSessionController->getSession()) {
+            $session = $browserSessionController->createSession();
         } else {
-            $this->getBrowserSessionController()->flagSessionForUpdate($session);
+            $browserSessionController->flagSessionForUpdate($session);
         }
-        $this->getBrowserSessionController()->saveCookie($session);
+        $browserSessionController->saveCookie($session);
         return $session;
     }
 
     protected function updateBrowserSession()
     {
+        $browserSessionController = $this->tryGetBrowserSessionController();
         $session = $this->getBrowserSession();
-        $this->getBrowserSessionController()->updateSession($session);
+        $browserSessionController->updateSession($session);
     }
 
     protected function flagBrowserSessionForUpdate()
     {
+        $browserSessionController = $this->tryGetBrowserSessionController();
         $session = $this->getBrowserSession();
-        $this->getBrowserSessionController()->flagSessionForUpdate($session);
+        $browserSessionController->flagSessionForUpdate($session);
     }
 }
